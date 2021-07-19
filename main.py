b80253298c12851ebd098ccc5e2cfb635590b22c
ttjjmm <tjm468@163.com>
2021-07-20 00:16:07 +0800

    backup

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import yaml
 
 
 if __name__ == '__main__':
-    file_path = '/home/ubuntu/Documents/pycharm/PaddleOCR/config/ppocr_mb.yaml'
+    file_path = '/home/tjm/Documents/python/pycharmProjects/PaddleOCR/config/ppocr_mb.yaml'
     cfg = yaml.load(open(file_path, 'rb'), Loader=yaml.Loader)
     print(cfg)
---
+++ model/arch/base_model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super(BaseModel, self).__init__()
+
+
+    def forward(self, x):
+        pass
